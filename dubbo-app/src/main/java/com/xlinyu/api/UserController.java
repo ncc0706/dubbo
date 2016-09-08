4aec1e7a4422bdd335607c549637b7c82539658b
ncc0706 <ncc0706@gmail.com>
2016-09-08 15:28:08 +0800

    spring boot

--- dubbo-app/src/main/java/com/xlinyu/api/UserController.java
+++ dubbo-app/src/main/java/com/xlinyu/api/UserController.java
@@ -4,12 +4,14 @@ import javax.annotation.Resource;
 
 import org.apache.log4j.Logger;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.xlinyu.user.model.User;
 import com.xlinyu.user.service.IUserService;
 
 @RestController
@@ -21,11 +23,18 @@ public class UserController {
 	@Resource
 	private IUserService userService;
 	
-	@RequestMapping(value = "/user/{message}", method = RequestMethod.GET)
+	@RequestMapping(value = "/user/say/{message}", method = RequestMethod.GET)
 	public ResponseEntity<String> sayHello(@PathVariable("message") String message){
 		logger.info("message: " + message);
 		String greeting = userService.sayHello(message);
 		return new ResponseEntity<String>(greeting, HttpStatus.OK);
+	}
+
+	@RequestMapping(value = "/user/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<User> getUser(@PathVariable("id") String id){
+		User user = userService.findOne(id);
+		logger.info(user);
+		return new ResponseEntity<User>(user, HttpStatus.OK);
 	}
 	
 }
