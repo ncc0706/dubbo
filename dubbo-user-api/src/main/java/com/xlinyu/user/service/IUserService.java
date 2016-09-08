9a80535b9283e635e470da1c6bed7dc7e310da98
ncc0706 <ncc0706@gmail.com>
2016-09-08 15:24:28 +0800

    用户服务接口

--- dubbo-user-api/src/main/java/com/xlinyu/user/service/IUserService.java
+++ dubbo-user-api/src/main/java/com/xlinyu/user/service/IUserService.java
@@ -1,7 +1,11 @@
 package com.xlinyu.user.service;
 
+import com.xlinyu.user.model.User;
+
 public interface IUserService {
 
 	public String sayHello(String name);
+	
+	public User findOne(String id);
 	
 }
